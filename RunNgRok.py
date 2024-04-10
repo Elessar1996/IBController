295272af43fe7a21d149c6f8e4b60981dc845872
Ali <ali.sarmadi6106@gmail.com>
2024-04-10 23:04:31 +0330

    Some Commits

--- RunNgRok.py
+++ RunNgRok.py
@@ -7,6 +7,6 @@ import signal
 
 if __name__ == '__main__':
 
-    subprocess.call('ngrok http --domain=loved-baboon-willingly.ngrok-free.app 5000', shell=True)
+    subprocess.call('ngrok http --domain=kind-amoeba-divine.ngrok-free.app 5000', shell=True)
 
 
